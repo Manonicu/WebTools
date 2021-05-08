f330f66abf13ec73978a515c7f56cc876a28c13d
Manon.icu <gemini0525@foxmail.com>
2021-05-08 20:22:22 +0800

    init

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+/**
+ * @ Author: ManonIcu
+ * @ Create Time: 2021/4/2 10:53
+ * @ Modified by: ManonIcu
+ * @ Modified time: 2021/4/2 10:53
+ * @ Description:
+ */
+module.exports ={
+	"arrowParens": "avoid",
+	"bracketSpacing": false,
+	"jsxBracketSameLine": false,
+	"jsxSingleQuote": false,
+	"printWidth": 120,
+	"semi": true,
+	"singleQuote": false,
+	"tabWidth": 4,
+	"trailingComma": "none",
+	"requirePragma": true,
+	"useTabs": true
+}
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+          <div className="grid grid-cols-3 gap-4">
+            <div className="bg-black h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold">1</div>
+            <div className="bg-black h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold">2</div>
+            <div className="bg-black h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold">3</div>
+            <div className="bg-black h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold">4</div>
+            <div className="bg-black h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold">5</div>
+            <div className="bg-black h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold">6</div>
+            <div className="bg-black h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold">7</div>
+            <div className="bg-black h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold">8</div>
+            <div className="bg-black h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold">9</div>
+          </div>
+  );
+}
+
+export default App;
